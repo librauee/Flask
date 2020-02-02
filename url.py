38a3f27e11f797498bb79749754dcdb1bbc483e3
librauee <870407139@qq.com>
2020-02-02 16:30:01 +0800

    动态构建url

---
+++ url.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  1 18:44:27 2020
+
+@author: Administrator
+"""
+
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+@app.route('/admin')
+def hello_admin():
+   return 'Hello Admin'
+
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+   return 'Hello %s as Guest' % guest
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+   if name =='admin':
+      return redirect(url_for('hello_admin'))
+   else:
+      return redirect(url_for('hello_guest',guest = name))
+
+
+if __name__ == '__main__':
+   app.run()
