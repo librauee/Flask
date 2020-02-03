ae67521517997ece4c11ca77f2b3eccf84fceb35
librauee <870407139@qq.com>
2020-02-03 22:00:18 +0800

    static

---
+++ static.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  1 21:59:45 2020
+
+@author: Administrator
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+   return render_template("index.html")
+
+if __name__ == '__main__':
+   app.run()
